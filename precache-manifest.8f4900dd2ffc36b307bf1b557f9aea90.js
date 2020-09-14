53612bbbdd6a600e8f2b6d3e79809c324e34d44c
anatolyefimov <efimo.tolik@gmail.com>
2020-09-14 10:58:32 +0300

    Updates

---
+++ precache-manifest.8f4900dd2ffc36b307bf1b557f9aea90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041dc67c77361d2c557ebfbb19dc78c1",
+    "url": "/donations.web/index.html"
+  },
+  {
+    "revision": "49608b70db9fbc113a86",
+    "url": "/donations.web/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "f5bd8605d8b8c04281b0",
+    "url": "/donations.web/static/css/main.001969e1.chunk.css"
+  },
+  {
+    "revision": "49608b70db9fbc113a86",
+    "url": "/donations.web/static/js/2.6fe17eb3.chunk.js"
+  },
+  {
+    "revision": "05f15178714e3a33848794d52baba9e8",
+    "url": "/donations.web/static/js/2.6fe17eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bd8605d8b8c04281b0",
+    "url": "/donations.web/static/js/main.e9c01120.chunk.js"
+  },
+  {
+    "revision": "0d58015d811190216c84",
+    "url": "/donations.web/static/js/runtime-main.34f85380.js"
+  },
+  {
+    "revision": "675177355b86f65f79d7bd1e6a0527c6",
+    "url": "/donations.web/static/media/Icon24Aim.67517735.svg"
+  }
+]);
